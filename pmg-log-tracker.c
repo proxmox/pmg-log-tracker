b1ff2f1351aabda42b5f762924fca2b75b009bd4
Dietmar Maurer <dietmar@proxmox.com>
2017-09-20 09:51:04 +0200

    only print CONNECT if we do not have a CLIENT

--- pmg-log-tracker.c
+++ pmg-log-tracker.c
@@ -1091,9 +1091,11 @@ qentry_print (LParser *parser, QEntry *qentry)
     printf ("CTIME: %08lX\n", parser->ctime);
     printf ("SIZE: %u\n", qentry->size);
 
-    if (se && se->connect) { printf ("CONNECT: %s\n", se->connect); }
-
-    if (qentry->client) { printf ("CLIENT: %s\n", qentry->client); }
+    if (qentry->client) {
+      printf ("CLIENT: %s\n", qentry->client);
+    } else if (se && se->connect) {
+      printf ("CONNECT: %s\n", se->connect);
+    }
 
     if (qentry->msgid) { printf ("MSGID: %s\n", qentry->msgid); }
 
