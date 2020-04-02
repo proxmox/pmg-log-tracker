093033c8cd556d402cd3cc35cb562d7d8f672e11
Mira Limbeck <m.limbeck@proxmox.com>
2020-04-02 16:57:14 +0200

    add a test case for recipient filtering
    Add a before queue test case to test recipient filtering. Previously
    all SEntry filter entries were printed because filtering failed. Now
    test that it only prints the specified ones.
    
    Signed-off-by: Mira Limbeck <m.limbeck@proxmox.com>

--- tests/tests_before_queue.rs
+++ tests/tests_before_queue.rs
@@ -136,3 +136,26 @@ fn before_queue_exclude_greylist_ndr() {
     let output_reader = BufReader::new(&output.stdout[..]);
     utils::compare_output(output_reader, expected_output);
 }
+
+#[test]
+fn before_queue_to() {
+    let output = Command::new(utils::log_tracker_path())
+        .arg("-vv")
+        .arg("-s")
+        .arg("1608300000")
+        .arg("-e")
+        .arg("1608302400")
+        .arg("-i")
+        .arg("tests/test_input_mixed")
+        .arg("-t")
+        .arg("pmgtes2")
+        .output()
+        .expect("failed to execute pmg-log-tracker");
+
+    let expected_file = File::open("tests/test_output_before_queue_to")
+        .expect("failed to open test_output");
+
+    let expected_output = BufReader::new(&expected_file);
+    let output_reader = BufReader::new(&output.stdout[..]);
+    utils::compare_output(output_reader, expected_output);
+}
