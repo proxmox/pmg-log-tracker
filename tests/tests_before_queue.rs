54a122d957ee98c6abe9c757861a637df960ff98
Mira Limbeck <m.limbeck@proxmox.com>
2020-04-24 17:21:50 +0200

    add test case for 'from' and 'to' together
    Updates the other test outputs' line numbers so the tests don't fail
    because of an addition in between.
    
    Signed-off-by: Mira Limbeck <m.limbeck@proxmox.com>

--- tests/tests_before_queue.rs
+++ tests/tests_before_queue.rs
@@ -180,3 +180,28 @@ fn before_queue_mixed_downstream() {
     let output_reader = BufReader::new(&output.stdout[..]);
     utils::compare_output(output_reader, expected_output);
 }
+
+#[test]
+fn before_queue_from_to() {
+    let output = Command::new(utils::log_tracker_path())
+        .arg("-vv")
+        .arg("-s")
+        .arg("1608300000")
+        .arg("-e")
+        .arg("1608302400")
+        .arg("-i")
+        .arg("tests/test_input_mixed")
+        .arg("-f")
+        .arg("mailtestsender3")
+        .arg("-t")
+        .arg("pmgtest")
+        .output()
+        .expect("failed to execute pmg-log-tracker");
+
+    let expected_file =
+        File::open("tests/test_output_before_queue_from_to").expect("failed to open test_output");
+
+    let expected_output = BufReader::new(&expected_file);
+    let output_reader = BufReader::new(&output.stdout[..]);
+    utils::compare_output(output_reader, expected_output);
+}
