d1dde9d3e1cad87bd31998189a16b428835742ba
Stoiko Ivanov <s.ivanov@proxmox.com>
2020-04-30 17:01:50 +0200

    add test for before queue dstatus
    Reviewed-By: Mira Limbeck <m.limbeck@proxmox.com>
    Tested-By: Mira Limbeck <m.limbeck@proxmox.com>
    Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

--- tests/tests_before_queue.rs
+++ tests/tests_before_queue.rs
@@ -159,3 +159,24 @@ fn before_queue_to() {
     let output_reader = BufReader::new(&output.stdout[..]);
     utils::compare_output(output_reader, expected_output);
 }
+
+#[test]
+fn before_queue_mixed_downstream() {
+    let output = Command::new(utils::log_tracker_path())
+        .arg("-v")
+        .arg("-s")
+        .arg("1608303600")
+        .arg("-e")
+        .arg("1608304200")
+        .arg("-i")
+        .arg("tests/test_input_mixed")
+        .output()
+        .expect("failed to execute pmg-log-tracker");
+
+    let expected_file = File::open("tests/test_output_before_queue_mixed_downstream")
+        .expect("failed to open test_output");
+
+    let expected_output = BufReader::new(&expected_file);
+    let output_reader = BufReader::new(&output.stdout[..]);
+    utils::compare_output(output_reader, expected_output);
+}
